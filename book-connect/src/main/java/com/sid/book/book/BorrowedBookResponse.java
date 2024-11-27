fee2ce713d3266de29d6d5de599ade4ee30ee7d0
siddharthxlodhi <siddharthxlodhi@gmail.com>
2024-11-27 12:00:40 +0530

    Initial commit

---
+++ book-connect/src/main/java/com/sid/book/book/BorrowedBookResponse.java
@@ -0,0 +1,20 @@
+package com.sid.book.book;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BorrowedBookResponse {
+
+    private Integer id;
+    private String title;
+    private String authorName;
+    private String isbn;
+    private double rate;
+    private String synopsis;
+    private boolean returned;
+    private boolean returnApproved;
+}
